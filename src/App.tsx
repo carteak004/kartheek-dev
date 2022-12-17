7294ef5e4c5ce19540d156764e841b6e75568599
Kartheek <carteak004@gmail.com>
2022-12-17 16:33:47 -0700

    removed absolute imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import About from '@components/about'
+import About from 'src/components/about'
 
 const App = () => {
 	return (
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
+const path = require('path')
+
 module.exports = {
 	resolve: {
 		extensions: ['.js', '.jsx', '.ts', '.tsx'],
+		alias: {
+			src: path.resolve(__dirname, 'src'),
+		},
 	},
 }
