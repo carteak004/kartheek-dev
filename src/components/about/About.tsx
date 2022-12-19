5a2eb0b32d907ca400e69ff8895b8015b6f87a84
Kartheek <carteak004@gmail.com>
2022-12-19 01:13:39 -0700

    added header

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -6,42 +6,48 @@ import SkillsSphere from './SkillsSphere'
 
 const About = () => {
 	return (
-		<div className='container'>
-			<div className='vertical-container'>
-				<div className='two-tab-container'>
-					<div>
-						<FaAward />
-						<h5>Experience</h5>
-						<h6>6+ years working</h6>
+		<>
+			<div className='header-container'>
+				<h5>Get to know</h5>
+				<h2>About me</h2>
+			</div>
+			<div className='container'>
+				<div className='vertical-container'>
+					<div className='two-tab-container'>
+						<div>
+							<FaAward />
+							<h5>Experience</h5>
+							<h6>6+ years working</h6>
+						</div>
+						<div>
+							<VscFolderLibrary />
+							<h5>Projects</h5>
+							<h6>10+ personal and professional</h6>
+						</div>
 					</div>
-					<div>
-						<VscFolderLibrary />
-						<h5>Projects</h5>
-						<h6>10+ personal and professional</h6>
-					</div>
+					<h4>
+						Results-driven Mobile Engineer skilled to design, develop, and
+						implement software programs for smartphones and other mobile devices
+						with deep knowledge of operating systems, such as Android and iOS
+						utilizing data-driven insights to guide development of programs and
+						apps that fill a user need utilizing active listening skills.
+					</h4>
 				</div>
-				<h4>
-					Results-driven Mobile Engineer skilled to design, develop, and
-					implement software programs for smartphones and other mobile devices
-					with deep knowledge of operating systems, such as Android and iOS
-					utilizing data-driven insights to guide development of programs and
-					apps that fill a user need utilizing active listening skills.
-				</h4>
+				<div>
+					<SkillsSphere />
+					<h6>
+						* Inspired by{' '}
+						<a
+							href='https://codepen.io/ryasan86/pen/bGpqdYV'
+							target='_blank'
+							rel='noreferrer'
+						>
+							Ryan Santos
+						</a>
+					</h6>
+				</div>
 			</div>
-			<div>
-				<SkillsSphere />
-				<h6>
-					* Inspired by{' '}
-					<a
-						href='https://codepen.io/ryasan86/pen/bGpqdYV'
-						target='_blank'
-						rel='noreferrer'
-					>
-						Ryan Santos
-					</a>
-				</h6>
-			</div>
-		</div>
+		</>
 	)
 }
 
