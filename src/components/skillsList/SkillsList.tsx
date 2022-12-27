397dc09ff9ceb4135f86096b4c229c7c34923740
Kartheek <carteak004@gmail.com>
2022-12-27 00:02:50 -0700

    added styling

--- src/components/skillsList/SkillsList.tsx
+++ src/components/skillsList/SkillsList.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react'
 import { BsFillPatchCheckFill } from 'react-icons/bs'
 import { Skill } from 'models/Skill'
+import './SkillsList.css'
 
 interface SkillsListProps {
 	header: string
@@ -9,13 +10,13 @@ interface SkillsListProps {
 
 const SkillsList: FC<SkillsListProps> = ({ header, skills }) => {
 	return (
-		<div className='experience-container'>
-			<h2>{header}</h2>
+		<div className='section-container'>
+			<h3>{header}</h3>
 			<div>
 				{skills.map((skill) => {
 					return (
-						<article>
-							<BsFillPatchCheckFill />
+						<article className='skill-article'>
+							<BsFillPatchCheckFill className='skill-icon' />
 							<div>
 								<h4>{skill.name}</h4>
 								<small>{skill.experience}</small>
--- src/pages/experience/Experience.tsx
+++ src/pages/experience/Experience.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import SkillsList from 'components/skillsList'
 import { backend, frontend } from 'constants/Skills'
+import './Experience.css'
 
 const Experience = () => {
 	return (
@@ -9,8 +10,10 @@ const Experience = () => {
 				<h5>What skills I have</h5>
 				<h2>My experience</h2>
 			</div>
-			<SkillsList header={'Frontend development'} skills={frontend} />
-			<SkillsList header={'Backend development'} skills={backend} />
+			<div className='skills-container'>
+				<SkillsList header={'Frontend development'} skills={frontend} />
+				<SkillsList header={'Backend development'} skills={backend} />
+			</div>
 		</section>
 	)
 }
