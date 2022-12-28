d733880787239cb5e1f09a1960c0258c670880b5
Kartheek <carteak004@gmail.com>
2022-12-27 23:34:27 -0700

    created services

--- src/pages/experience/Experience.tsx
+++ src/pages/experience/Experience.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
+import JobRole from 'components/jobRole'
 import SectionHead from 'components/sectionHead'
-import SkillsList from 'components/skillsList'
-import { backend, frontend } from 'constants/Skills'
+import { Krise, Landing, TCS } from 'constants/Jobs'
 import './Experience.css'
 
 const Experience = () => {
 	return (
 		<section id='experience'>
-			<SectionHead lineOne='What skills I have' lineTwo='My experience' />
-			<div className='skills-container'>
-				<SkillsList header={'Frontend development'} skills={frontend} />
-				<SkillsList header={'Backend development'} skills={backend} />
+			<SectionHead lineOne='What I contributed' lineTwo='My experience' />
+			<div className='experience-container'>
+				<JobRole job={Landing} />
+				<JobRole job={Krise} />
+				<JobRole job={TCS} />
 			</div>
 		</section>
 	)
