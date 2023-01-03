cd80b585621df54b4b884eb375cf43c724eba5e4
Kartheek <carteak004@gmail.com>
2023-01-02 21:57:34 -0700

    added styling and media queries

--- src/pages/footer/Footer.tsx
+++ src/pages/footer/Footer.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { BsLinkedin } from 'react-icons/bs'
+import { FaGithub } from 'react-icons/fa'
+import { FiInstagram } from 'react-icons/fi'
 import './Footer.css'
 
 const Footer = () => {
@@ -33,6 +36,53 @@ const Footer = () => {
 					<a href='#contact'>Contact</a>
 				</li>
 			</ul>
+
+			<div className='footer-socials'>
+				<a
+					href='https://www.linkedin.com/in/kchintalapati/'
+					target='_blank'
+					rel='noreferrer'
+				>
+					<BsLinkedin />
+				</a>
+				<a
+					href='https://github.com/carteak004'
+					target='_blank'
+					rel='noreferrer'
+				>
+					<FaGithub />
+				</a>
+				<a
+					href='https://www.instagram.com/carteak004/'
+					target='_blank'
+					rel='noreferrer'
+				>
+					<FiInstagram />
+				</a>
+			</div>
+
+			<div className='footer-inspirations'>
+				<small>
+					*Skills sphere adapted from{'  '}
+					<a
+						href='https://codepen.io/ryasan86/pen/bGpqdYV'
+						target='_blank'
+						rel='noreferrer'
+					>
+						Ryan Santos
+					</a>
+				</small>
+				<small>
+					**Website and design adapted from{'  '}
+					<a
+						href='https://www.youtube.com/watch?v=G-Cr00UYokU&t=4163s'
+						target='_blank'
+						rel='noreferrer'
+					>
+						EGATOR
+					</a>
+				</small>
+			</div>
 		</footer>
 	)
 }
