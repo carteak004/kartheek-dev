7dab6a79bdde782fa5383926da053339c10b95d7
Kartheek Chintalapati <carteak004@gmail.com>
2023-07-09 21:31:57 -0600

    [Feature] Added new updates as of July 2023 (#28)
    * add scrolling to jobs
    
    * added recommendations and projects
    
    * added keys
    
    * updated resume

--- src/components/jobRole/JobRole.tsx
+++ src/components/jobRole/JobRole.tsx
@@ -5,11 +5,12 @@ import './JobRole.css'
 
 interface JobRoleProps {
 	job: Job
+	cssClass: 'first' | 'last' | ''
 }
 
-const JobRole: FC<JobRoleProps> = ({ job }) => {
+const JobRole: FC<JobRoleProps> = ({ job, cssClass }) => {
 	return (
-		<article className='job-role'>
+		<article className={`job-role ${cssClass}`.trim()}>
 			<div className='company-name'>
 				<h3>{job.companyName}</h3>
 			</div>
